928537e54214a17b67614bc8bb27c9e11fc773cf
kkcchh9999 <kkcchh9999@gmail.com>
2023-02-27 00:58:11 +0900

    10812 풀어야함

---
+++ 심화1/10812.py
@@ -0,0 +1,5 @@
+n, m = map(int, input().split(" "))
+
+arr = [i+1 for i in range(n)]
+
+
---
+++ 심화1/2444.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(1, n+1) : 
+    for j in range(i, n) : 
+        print(" ", end = "")
+    for j in range(0, 2 * i -1) : 
+        print("*", end = "")
+    print()
+
+for i in range(n-1, 0, -1) : 
+    for j in range(i, n) : 
+        print(" ", end = "")
+    for j in range(0, 2 * i - 1) : 
+        print("*", end = "")
+    print()
+    
